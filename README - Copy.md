e6f49e189928cd7f7e66367cc40e703d6e1c0ef5
Pintu Kumar <64580363+pintu544@users.noreply.github.com>
2023-04-22 09:27:51 +0530

    All functionality added

---
+++ README - Copy.md
@@ -0,0 +1,28 @@
+<h1 id="guruji-astrologer-frontend-project--readme.md">Guruji Astrologer Frontend Project  <a href="http://readme.md/">Readme.md</a></h1>
+<h2 id="project-description">Project Description</h2>
+<p>This project is the frontend for the “Astrology for Clarity” website, which provides users with access to premium astrologers who have expertise in Vedic astrology, Vaastu, and Mantra therapy. The website offers solutions for a wide range of problems faced by users, ranging from relationship issues to career advice.</p>
+<h2 id="user-testimonial">User Testimonial</h2>
+<p>“I had been struggling with my career for years, but after consulting with Guruji, I finally found clarity and direction. His insights into my birth chart were incredibly accurate, and his guidance has helped me make some significant positive changes in my life.”</p>
+<h2 id="premium-astrologers">Premium Astrologers</h2>
+<p>Our website features a team of highly experienced and knowledgeable astrologers who specialize in different areas of astrology. We carefully select our astrologers based on their qualifications, experience, and expertise to ensure that our users receive the best possible advice and guidance.</p>
+<h2 id="many-problems-one-solution">Many Problems One Solution</h2>
+<p>Whether you’re facing personal or professional issues, our astrologers are here to help. From relationship problems to financial struggles, our astrologers can offer guidance and solutions to help you overcome any obstacle in your life.</p>
+<h2 id="user-reviews">User Reviews</h2>
+<p>“Consulting with Guruji was one of the best decisions I’ve made. His predictions and remedies have been spot on, and I’ve seen significant improvements in all areas of my life since following his advice.”</p>
+<p>“I was skeptical about astrology at first, but after consulting with one of the astrologers on this website, I’m now a believer. Their insights into my life were incredibly accurate, and their guidance has helped me make some important decisions.”</p>
+<p>“Highly recommend this website to anyone looking for guidance and clarity in their life. The astrologers are top-notch, and the customer support team is friendly and helpful.”</p>
+<p>To start the React project, you will need to have Node.js and npm installed on your computer. Here are the steps to create a new React project:</p>
+<ol>
+<li>Open a terminal and navigate to the directory where you want to create the project.</li>
+<li>Run the command  <code>npx create-react-app my-app</code>  to create a new React project named “my-app”.</li>
+<li>Navigate into the project directory by running  <code>cd my-app</code>.</li>
+<li>Run the command  <code>npm start</code>  to start the development server.</li>
+</ol>
+<p>Once the development server is running, you can begin building out the functionality of the Guruji Astrologer website. The basic functionality of the site should include:</p>
+<ol>
+<li>User authentication: Users should be able to create an account and log in to access their account information and to consult with astrologers.</li>
+<li>Astrologer profiles: Astrologers should have their own profiles with information about their qualifications, areas of expertise, and availability.</li>
+<li>Consultation booking: Users should be able to book consultations with astrologers based on their availability and areas of expertise.</li>
+<li>Payment processing: Users should be able to pay for consultations securely through the site.</li>
+<li>Communication platform: Users and astrologers should be able to communicate through a messaging system or video call platform within the site.</li>
+</ol>
---
+++ src - Copy/data/astrolgersdata.js
@@ -0,0 +1,101 @@
+export const astrologers = [
+  {
+    name: "Astrologer Ramraj",
+    rating: "4.5",
+    experience: "10+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "10",
+    image:
+      "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=740&t=st=1682060979~exp=1682061579~hmac=b7bb8616cec4dbd307bce2bcdd5df2e13e51571d34d59e5af36184522d92002e",
+  },
+  {
+    name: "Astrologer Surya",
+    rating: "4.0",
+    experience: "1+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "12",
+    image:
+      "https://img.freepik.com/free-photo/handsome-businessman-looking-camera_657883-499.jpg?w=360&t=st=1682061052~exp=1682061652~hmac=84b2bcbd50b1ed65eeca73d567430f1259bba81880a73428d03577a47693da68",
+  },
+  {
+    name: "Astrologer Mukesh",
+    rating: "4.2",
+    experience: "5+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "15",
+    image:
+      "https://img.freepik.com/free-photo/smiling-confident-man-showing-thumbs-up-with-excited-face-like-something-awesome-approving-product-standing-against-blue-background_1258-73715.jpg?w=740&t=st=1682061095~exp=1682061695~hmac=d5c5e380c9755a4213268299788611bd8bb4a06b55e218f1e6eb236e7001c2a4",
+  },
+  {
+    name: "Astrologer Raj",
+    rating: "4.1",
+    experience: "10+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "14",
+    image:
+      "https://img.freepik.com/free-photo/portrait-serious-smiling-modern-indian-man-near-office-building_496169-2890.jpg?w=740&t=st=1682061139~exp=1682061739~hmac=35fab6bb34b2bbf4e730e84b6a7139902bf3e2f95f949c53d02bffff7ca64213",
+  },
+  {
+    name: "Astrologer Kamlesh",
+    rating: "3.9",
+    experience: "1+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "2",
+    image:
+      "https://img.freepik.com/free-photo/full-length-portrait-confident-happy-man_171337-4818.jpg?w=360&t=st=1682061180~exp=1682061780~hmac=ad22c73f822b5172eecf1e7b8d2645c78b4fe1baa2662a8459709c14e07f4445",
+  },
+  {
+    name: "Astrologer Suresh",
+    rating: "4.5",
+    experience: "3+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "5",
+    image:
+      "https://img.freepik.com/free-photo/smiling-man-white-shirt-typing-text-message-scrolling-feed-social-network-using-smartphone-gray_171337-634.jpg?w=740&t=st=1682061212~exp=1682061812~hmac=3a9d6c5e05d1fd5bc2acd32904c9862d994bc77eea4784f24546ca19a18a4633",
+  },
+  {
+    name: "Astrologer Shyam",
+    rating: "4.5",
+    experience: "10+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "20",
+    image:
+      "https://img.freepik.com/free-photo/portrait-beautiful-young-woman-standing-grey-wall_231208-10760.jpg?size=626&ext=jpg",
+  },
+  {
+    name: "Astrologer Kunal",
+    rating: "4.5",
+    experience: "20+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "100",
+    image:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+  },
+  {
+    name: "Astrologer Priya",
+    rating: "4.5",
+    experience: "10+ Years",
+    status: "online",
+    specialities: "Love, Business, LIfe",
+    skills: "Vedic Astrology, Kundali",
+    price: "30",
+    image:
+      "https://img.freepik.com/free-photo/picture-dissatisfied-grumpy-young-afro-american-woman-with-long-straight-hair-expressing-her-disagreement-keeping-arms-folded-looking-with-serious-skeptical-facial-expression_344912-1026.jpg?size=626&ext=jpg&ga=GA1.1.1289143428.1681738162&semt=sph",
+  },
+];
